9851b19ccca0571bf45ea85b5ee8038d54390c5c
davinicollas <davi.nicollas@hotmail.com>
2021-04-08 11:31:45 -0300

    Correção do projeto

---
+++ src/main/java/com/example/webServiceJPA/WebServiceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.webServiceJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webServiceJPA/WebServiceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webServiceJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
