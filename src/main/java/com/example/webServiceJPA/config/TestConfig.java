c288ae60cc83a7e31a77b92bef3f1139277c25bf
davinicollas <davi.nicollas@hotmail.com>
2021-04-13 09:15:10 -0300

    Associação um para um entidade de pagamento

--- src/main/java/com/example/webServiceJPA/config/TestConfig.java
+++ src/main/java/com/example/webServiceJPA/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.webServiceJPA.enity.Category;
 import com.example.webServiceJPA.enity.Order;
 import com.example.webServiceJPA.enity.OrderItem;
+import com.example.webServiceJPA.enity.Payment;
 import com.example.webServiceJPA.enity.Products;
 import com.example.webServiceJPA.enity.User;
 import com.example.webServiceJPA.enity.enums.OrderStatus;
@@ -78,6 +79,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		OrdemItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	
 	}
 	
--- src/main/java/com/example/webServiceJPA/enity/Order.java
+++ src/main/java/com/example/webServiceJPA/enity/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,11 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.webServiceJPA.enity.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order")
@@ -38,6 +39,8 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	@OneToOne(mappedBy ="order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -88,6 +91,14 @@ public class Order implements Serializable {
 	
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
